d45ee8e7b7234528504edf9c4727a7a8069189e7
PolinKartav <polyakarpushina@yandex.ru>
2023-04-22 16:52:08 +0300

    Spring9 - 2

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,21 +1,12 @@
 package ru.yandex.practicum.filmorate;
 
-import org.apache.el.util.Validation;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.ldap.embedded.EmbeddedLdapProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FilmorateApplicationTests {
-	Validation validation;
 
-	@Test
-	void contextLoads() {
-
-
-	}
-
-
-
-
+    @Test
+    void contextLoads() {
+    }
 }
