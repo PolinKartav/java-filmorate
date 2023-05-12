09cbf2029b8f8e65041a19e8ca04f088ec0122c7
PolinKartav <polyakarpushina@yandex.ru>
2023-05-12 11:34:24 +0300

    style: add empty lines

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -5,7 +5,10 @@ import java.util.List;
 
 public interface UserStorage {
     List<User> getAllUsers();
+
     User getUser(long userId);
+
     User createUser(User user);
+    
     User updateUser(User user);
 }
