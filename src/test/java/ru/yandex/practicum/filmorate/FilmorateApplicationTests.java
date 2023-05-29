6c386ee11f796d21f1421ecfc340a22fbc9b5fd7
PolinKartav <polyakarpushina@yandex.ru>
2023-05-29 20:34:05 +0300

    delete: unused import;

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -18,7 +18,6 @@ import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
